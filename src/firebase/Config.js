0cf2ba90e743799dea0d523b6ed1bfce4b1e2087
EMMANUEL <dammylola299@gmail.com>
2023-01-24 14:33:42 +0100

    Added firebase configuration

---
+++ src/firebase/Config.js
@@ -0,0 +1,16 @@
+// All firebase imports
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDJRAHn2pNxl7UgG42bvYOECW0vdhRXuAQ",
+  authDomain: "finance-tracker-project-b77f5.firebaseapp.com",
+  projectId: "finance-tracker-project-b77f5",
+  storageBucket: "finance-tracker-project-b77f5.appspot.com",
+  messagingSenderId: "189730357376",
+  appId: "1:189730357376:web:f653a922cc1e716ad96665"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
