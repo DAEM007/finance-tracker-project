f3c68f78dc8b91e48f75d94737185a4718b1a333
EMMANUEL <dammylola299@gmail.com>
2023-01-27 12:37:17 +0100

    used AuthContextprovider for the app component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
+// react imports
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// styles imports
 import './index.css';
 import App from './App';
+// context imports
+import { AuthContextProvider } from './context/AuthContext';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
   </React.StrictMode>
 );
 
