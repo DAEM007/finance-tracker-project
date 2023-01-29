c3f3fccb4dbe7ce1e81609880da8f6002c1eac5b
EMMANUEL <dammylola299@gmail.com>
2023-01-29 03:45:16 +0100

    used the logout function from the useLogout hook to log users out

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,25 @@
 // All react imports
 import { Link } from "react-router-dom";
+// All hooks import
+import useLogout from "../hooks/useLogout";
 // All styles import
 import styles from "./NavBar.module.css";
 
 
 export default function NavBar() {
+  const { logout } = useLogout();
+
+
   return (
     <nav className={styles.navbar}>
         <ul>
             <li className={styles.title}>mymoney</li>
             <li><Link to="/login">Login</Link></li>
             <li><Link to="/signup">Signup</Link></li>
+
+            <li>
+              <button className="btn" onClick={logout}>Logout</button>
+            </li>
         </ul>
     </nav>
   )
