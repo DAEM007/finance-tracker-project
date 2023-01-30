92ebd9659a0a941d3ba33a6e2946b6a4e6ac7190
EMMANUEL <dammylola299@gmail.com>
2023-01-30 03:36:05 +0100

    conditionally showed user contents based on the AuthContext

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,24 +2,38 @@
 import { Link } from "react-router-dom";
 // All hooks import
 import useLogout from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 // All styles import
 import styles from "./NavBar.module.css";
 
 
+
 export default function NavBar() {
   const { logout } = useLogout();
+  const { user } = useAuthContext();
 
 
   return (
     <nav className={styles.navbar}>
         <ul>
             <li className={styles.title}>mymoney</li>
-            <li><Link to="/login">Login</Link></li>
-            <li><Link to="/signup">Signup</Link></li>
 
-            <li>
-              <button className="btn" onClick={logout}>Logout</button>
-            </li>
+            {!user && (
+              <>
+                <li><Link to="/login">Login</Link></li>
+                <li><Link to="/signup">Signup</Link></li>
+              </>
+            )}
+          
+            {user && (
+              <>
+                <li>Hello, { user.displayName }</li>
+                <li>
+                  <button className="btn" onClick={logout}>Logout</button>
+                </li>
+              </>
+            )}
+
         </ul>
     </nav>
   )
