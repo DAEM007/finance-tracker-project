298307ccfede26580cd6f02e7a934209bf9f2593
EMMANUEL <dammylola299@gmail.com>
2023-01-30 04:52:47 +0100

    used the authIsReady property to render components in the App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 // All react imports
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// All hooks import
+import { useAuthContext } from "./hooks/useAuthContext";
+// All component imports
 import NavBar from "./components/NavBar";
-// All pages imports
+// All pages import
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
 
 
 function App() {
+  const { authIsReady } = useAuthContext();
+
   return (
     <div className="App">
-      <Router>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={ <Home /> } />
-          <Route path="/signup" element={ <Signup /> } />
-          <Route path="/login" element={ <Login /> } />
-        </Routes>
-      </Router>
+      { authIsReady && (
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={ <Home /> } />
+            <Route path="/signup" element={ <Signup /> } />
+            <Route path="/login" element={ <Login /> } />
+          </Routes>
+        </Router>
+      ) }
     </div>
   );
 }
