7c7c3a0e92557aa2e5e16e4dc51478c255a38ba1
EMMANUEL <dammylola299@gmail.com>
2023-01-31 02:39:37 +0100

    created a Transaction from component

---
+++ src/pages/home/TransactionForm.js
@@ -0,0 +1,48 @@
+// All react imports
+import { useState } from "react";
+
+
+const TransactionForm = () => {
+    const [name, setName] = useState('');
+    const [amount, setAmount] = useState('');
+
+    // function to handle submission of the form
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log({
+            name,
+            amount
+        });
+    }
+
+    return (
+        <>
+            <h3>Add new transaction</h3>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <span>Name:</span>
+                    <input 
+                        type="text"
+                        onChange={ (e) => setName(e.target.value) }
+                        value={ name }
+                        required
+                    />
+                </label>
+
+                <label>
+                    <span>Amount($):</span>
+                    <input 
+                        type="text"
+                        onChange={ (e) => setAmount(e.target.value) }
+                        value={ amount }
+                        required
+                    />
+                </label>
+                <button>Add transaction</button>
+            </form>
+        </>
+    );
+}
+ 
+export default TransactionForm;
