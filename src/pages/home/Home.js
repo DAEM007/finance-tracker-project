d52e35fc605d428926186502fe195752621b6173
EMMANUEL <dammylola299@gmail.com>
2023-01-31 11:09:38 +0100

    used AuthContext to get user object and pass in Id as prop to the Transaction component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,17 +1,20 @@
 // All styles import
+import { useAuthContext } from "../../hooks/useAuthContext";
 import styles from "./Home.module.css";
 // All component imports
 import TransactionForm from "./TransactionForm";
 
 
 export default function Home() {
+  const { user } = useAuthContext();
+
   return (
     <div className={styles.container}>
       <div className={styles.content}>
         Transaction list
       </div>
       <div className={styles.sidebar}>
-        <TransactionForm />
+        <TransactionForm userId={ user.uid } />
       </div>
     </div>
   )
