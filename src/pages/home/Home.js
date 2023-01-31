63d67934b435e4e4c2c4f267f0b0a032226d3b17
EMMANUEL <dammylola299@gmail.com>
2023-01-31 02:37:54 +0100

    Added the Transaction form to the Home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,18 @@
+// All styles import
 import styles from "./Home.module.css";
+// All component imports
+import TransactionForm from "./TransactionForm";
+
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        content
+      </div>
+      <div className={styles.sidebar}>
+        <TransactionForm />
+      </div>
+    </div>
   )
 }
