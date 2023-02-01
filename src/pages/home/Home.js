bb4f66f1e8546087fb5c92c95ad636d70f8f256c
EMMANUEL <dammylola299@gmail.com>
2023-02-01 17:11:41 +0100

    used collection hook and TransactionList component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,17 +1,22 @@
 // All styles import
-import { useAuthContext } from "../../hooks/useAuthContext";
 import styles from "./Home.module.css";
 // All component imports
 import TransactionForm from "./TransactionForm";
+import TransactionList from "./TransactionList";
+// All hooks import
+import { useAuthContext } from "../../hooks/useAuthContext";
+import useCollection from "../../hooks/useCollection";
 
 
 export default function Home() {
   const { user } = useAuthContext();
+  const { documents, error } = useCollection('transactions');
 
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        Transaction list
+        { error && <p>{ error }</p> }
+        { documents && < TransactionList transactions={documents} /> }
       </div>
       <div className={styles.sidebar}>
         <TransactionForm userId={ user.uid } />
@@ -19,3 +24,4 @@ export default function Home() {
     </div>
   )
 }
+
